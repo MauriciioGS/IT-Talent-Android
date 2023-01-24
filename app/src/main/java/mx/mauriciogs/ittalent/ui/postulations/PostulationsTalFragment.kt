0b5462f083684bac0fa175a6f8f1ce1d70dce3ee
MauriciioGS <mauricaz@outlook.com>
2023-01-24 02:53:21 -0600

    [master][UI] Fix postulations

--- app/src/main/java/mx/mauriciogs/ittalent/ui/postulations/PostulationsTalFragment.kt
+++ app/src/main/java/mx/mauriciogs/ittalent/ui/postulations/PostulationsTalFragment.kt
@@ -3,6 +3,7 @@ package mx.mauriciogs.ittalent.ui.postulations
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,25 +15,34 @@ import mx.mauriciogs.ittalent.core.BaseFrag
 import mx.mauriciogs.ittalent.core.extensions.*
 import mx.mauriciogs.ittalent.databinding.FragmentPostulationsTalBinding
 import mx.mauriciogs.ittalent.domain.jobs.Job
-import mx.mauriciogs.ittalent.ui.jobs.adapters.JobsAdapter
-import mx.mauriciogs.ittalent.ui.jobs.adapters.PastJobsAdapter
+import mx.mauriciogs.ittalent.ui.connectivity.LostConnViewModel
+import mx.mauriciogs.ittalent.ui.connectivity.LostConnectionFragment
+import mx.mauriciogs.ittalent.ui.init.InitViewModel
+import mx.mauriciogs.ittalent.ui.postulations.adapters.PostulPastTalentAdapter
 import mx.mauriciogs.ittalent.ui.postulations.adapters.PostulTalentAdapter
+import mx.mauriciogs.ittalent.ui.welcome.WelcomeFragment
 
 private const val PROCESS_JOB_FINISHED = 4
+private val lostConnBottomSheetTag: String = LostConnectionFragment::class.java.simpleName
 
 class PostulationsTalFragment: BaseFrag<FragmentPostulationsTalBinding>(R.layout.fragment_postulations_tal) {
 
     private lateinit var mBinding: FragmentPostulationsTalBinding
 
     private val postulationsTalViewModel: PostulationsTalViewModel by activityViewModels()
+    private val initViewModel : InitViewModel by viewModels() {
+        InitViewModel.MainVMFactory(requireActivity().application)
+    }
+    private val lostConnViewModel : LostConnViewModel by activityViewModels()
 
     private var userEmail = String.empty()
 
     override fun FragmentPostulationsTalBinding.initialize() {
         mBinding = this
-        postulationsTalViewModel.getProfile()
+        initViewModel.monitorStateConnection()
         showToolBar(true)
         initObservers()
+        postulationsTalViewModel.getProfile()
     }
 
     private fun initObservers() {
@@ -41,6 +51,8 @@ class PostulationsTalFragment: BaseFrag<FragmentPostulationsTalBinding>(R.layout
             if (it.exception != null) requireActivity().snackbar(it.exception.message).showWarning()
             if (it.setUI != null) initUI(it.setUI)
         }
+        initViewModel.isConnected.observe(viewLifecycleOwner) { isConnected -> if (!isConnected) openLostConnDialog() }
+        lostConnViewModel.isUiEnabled.observe(viewLifecycleOwner) { if (it) dismissLostConnDialog() }
     }
 
     private fun initUI(jobs: MutableList<Job>) {
@@ -68,18 +80,30 @@ class PostulationsTalFragment: BaseFrag<FragmentPostulationsTalBinding>(R.layout
     }
 
     private fun initRecyclerPast(jobs: MutableList<Job>) {
+        println(jobs)
         mBinding.noDataAnim2.visibility = View.GONE
         mBinding.rvPast.apply {
             visibility = View.VISIBLE
-            layoutManager = LinearLayoutManager(requireActivity()).apply { orientation = RecyclerView.HORIZONTAL }
-            adapter = PostulTalentAdapter(jobs, this@PostulationsTalFragment)
+            layoutManager = LinearLayoutManager(requireActivity())
+            adapter = PostulPastTalentAdapter(jobs, this@PostulationsTalFragment)
         }
     }
 
     fun onClickItem(item: Job) {
-        // TODO: abrir empleo desde postulaciones y contactar al reclutador
         findNavControllerSafely()
             ?.safeNavigate(PostulationsTalFragmentDirections.actionPostulationsTalFragmentToStatusPostulFragment(item))
+    }
+
+    private fun openLostConnDialog() = LostConnectionFragment.newInstance().run {
+        this@PostulationsTalFragment.childFragmentManager.executePendingTransactions()
+        if(!this@PostulationsTalFragment.findChildFragmentByTag(WelcomeFragment.lostConnBottomSheetTag)?.isAdded.orDefault())
+            show(this@PostulationsTalFragment.childFragmentManager, WelcomeFragment.lostConnBottomSheetTag)
+    }
+
+    private fun dismissLostConnDialog() = this@PostulationsTalFragment.findChildFragmentByTag(
+        lostConnBottomSheetTag
+    )?.asDialogFragment()?.run {
+        dismiss()
     }
 
 }
---
+++ app/src/main/java/mx/mauriciogs/ittalent/ui/postulations/adapters/PostulPastTalentAdapter.kt
@@ -0,0 +1,110 @@
+package mx.mauriciogs.ittalent.ui.postulations.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import mx.mauriciogs.ittalent.R
+import mx.mauriciogs.ittalent.core.extensions.showWarning
+import mx.mauriciogs.ittalent.core.extensions.snackbar
+import mx.mauriciogs.ittalent.core.extensions.toast
+import mx.mauriciogs.ittalent.databinding.JobForTalentCardBinding
+import mx.mauriciogs.ittalent.databinding.JobPostedCardBinding
+import mx.mauriciogs.ittalent.databinding.JobProcessTalentCardBinding
+import mx.mauriciogs.ittalent.domain.jobs.Job
+import mx.mauriciogs.ittalent.ui.jobs.JobsFragment
+import mx.mauriciogs.ittalent.ui.postulations.PostulationsTalFragment
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import org.joda.time.Days
+import org.joda.time.Hours
+import org.joda.time.Minutes
+
+private const val PROCESS_JOB_STAGE1 = 0
+private const val PROCESS_JOB_STAGE2 = 1
+private const val PROCESS_JOB_STAGE3 = 2
+private const val PROCESS_JOB_FINISHED = 4
+
+class PostulPastTalentAdapter(private val jobsList: List<Job>, val fragment: Fragment): RecyclerView.Adapter<PostulPastTalentAdapter.ViewHolder>() {
+
+    class ViewHolder(val itemBinding: JobProcessTalentCardBinding): RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(job: Job) {
+            Log.d("AKI", "${job.job}")
+            itemBinding.tvJobName.text = job.job
+            itemBinding.tvEnterprise.text = job.enterprise
+            itemBinding.tvCity.text = job.location
+            itemBinding.tvModal.text = itemBinding.root.context.getString(R.string.txt_rvjobs_modalidad, job.mode)
+            itemBinding.tvType.text = itemBinding.root.context.getString(R.string.txt_rvjobs_tipo, job.type)
+            itemBinding.tvTime.text = getTime(job.date, job.time)
+            when (job.status) {
+                PROCESS_JOB_STAGE1 -> {
+                    itemBinding.ivStatus.text = itemBinding.root.context.getString(R.string.txt_postulaciones_stg1)
+                    itemBinding.ivStatus.setTextColor(itemBinding.root.context.getColor(R.color.info))
+                }
+                PROCESS_JOB_STAGE2 -> {
+                    itemBinding.ivStatus.text = itemBinding.root.context.getString(R.string.txt_postulaciones_stg2)
+                    itemBinding.ivStatus.setTextColor(itemBinding.root.context.getColor(R.color.warning))
+                }
+                PROCESS_JOB_STAGE3 -> {
+                    itemBinding.ivStatus.text = itemBinding.root.context.getString(R.string.txt_postulaciones_stg3)
+                    itemBinding.ivStatus.setTextColor(itemBinding.root.context.getColor(R.color.success))
+                }
+                PROCESS_JOB_FINISHED -> {
+                    itemBinding.ivStatus.text = itemBinding.root.context.getString(R.string.txt_postulaciones_stgend)
+                    itemBinding.ivStatus.setTextColor(itemBinding.root.context.getColor(R.color.error))
+                }
+
+            }
+        }
+
+        private fun getTime(dateString: String?, time: String?): String {
+            if (dateString != null && time != null) {
+                val date = dateString.split("-")
+                val hour = time.split(":")
+
+                val year = date[0].toInt()
+                val month = date[1].toInt()
+                val day = date[2].toInt()
+
+                val hours = hour[0].toInt()
+                val minutes = hour[1].toInt()
+
+                val dateOfPost = DateTime(year, month, day, hours, minutes, 0, 0)
+                var current = DateTime(DateTimeZone.UTC)
+
+                try {
+                    current = DateTime.now().withZone(DateTimeZone.forID("America/Mexico_City"))
+                } catch (e: Exception){
+                    e.printStackTrace()
+                }
+
+                val timeInDays = Days.daysBetween(dateOfPost, current)
+                val timeInHours = Hours.hoursBetween(dateOfPost, current)
+                val timeInMinutes = Minutes.minutesBetween(dateOfPost, current)
+
+                return if (timeInMinutes.minutes < 60) itemBinding.root.context.getString(R.string.txt_rvjobs_time_min, timeInMinutes.minutes)
+                else if (timeInHours.hours < 24) itemBinding.root.context.getString(R.string.txt_rvjobs_time_horas, timeInHours.hours)
+                else itemBinding.root.context.getString(R.string.txt_rvjobs_time_dias, timeInDays.days)
+            }
+            return ""
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(fragment.requireActivity())
+        val binding = JobProcessTalentCardBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.itemView.setOnClickListener {
+            fragment.requireActivity().snackbar("NO se pueden abrir procesos pasados").showWarning()
+        }
+        holder.bind(jobsList[position])
+    }
+
+    override fun getItemCount(): Int = jobsList.size
+
+}
