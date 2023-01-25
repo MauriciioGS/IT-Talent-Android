4d3e3a3d37fbddf7a12c745ae261ed04a55b249d
MauriciioGS <mauricaz@outlook.com>
2023-01-24 22:27:38 -0600

    [master][UI] Fix New enterprise

--- app/src/main/java/mx/mauriciogs/ittalent/ui/authentication/signup/RegisterFragmentEnterpriseRecruit.kt
+++ app/src/main/java/mx/mauriciogs/ittalent/ui/authentication/signup/RegisterFragmentEnterpriseRecruit.kt
@@ -1,17 +1,17 @@
 package mx.mauriciogs.ittalent.ui.authentication.signup
 
+import android.app.Activity
 import android.content.Intent
 import android.util.Log
 import android.widget.AutoCompleteTextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 import mx.mauriciogs.ittalent.R
 import mx.mauriciogs.ittalent.core.BaseFrag
-import mx.mauriciogs.ittalent.core.extensions.findNavControllerSafely
-import mx.mauriciogs.ittalent.core.extensions.safeNavigate
-import mx.mauriciogs.ittalent.core.extensions.showError
-import mx.mauriciogs.ittalent.core.extensions.snackbar
+import mx.mauriciogs.ittalent.core.extensions.*
 import mx.mauriciogs.ittalent.databinding.FragmentRegisterEnterpriseBinding
 import mx.mauriciogs.ittalent.ui.main.MainActivity
 
@@ -73,7 +73,8 @@ class RegisterFragmentEnterpriseRecruit : BaseFrag<FragmentRegisterEnterpriseBin
 
     private fun showError(exception: Exception) {
         snackbar(exception.message).showError()
-        findNavControllerSafely()?.safeNavigate(RegisterFragmentEnterpriseRecruitDirections.actionRegisterFragmentEnterpriseRecruitToSignInFragment())
+        findNavControllerSafely()
+            ?.safeNavigate(RegisterFragmentEnterpriseRecruitDirections.actionRegisterFragmentEnterpriseRecruitToSignInFragment())
     }
 
     private fun emptyField(editText: TextInputEditText, errorId: Int) {
@@ -87,7 +88,36 @@ class RegisterFragmentEnterpriseRecruit : BaseFrag<FragmentRegisterEnterpriseBin
     }
 
     private fun showInfoDialog() {
+        MaterialAlertDialogBuilder(requireActivity(), R.style.MyDialog)
+            .setTitle("Registrar una empresa")
+            .setMessage("Si tu empresa es nueva por aquí y deseas registrarla, envíanos un email con más " +
+                    "información de la empresa como tados fiscales y el logo de la empresa para personalizar tus ofertas")
+            .setPositiveButton(R.string.btn_envia_correo) { dialog, _ ->
+                sendEmail(arrayOf("hector.garcias@ingenieria.unam.edu"), "Registro de nueva empresa")
+                dialog.dismiss()
+            }
+            .setNegativeButton(R.string.btn_cancelar) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
 
+    private fun sendEmail(to: Array<String>, asunto: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_EMAIL, to)
+            putExtra(Intent.EXTRA_SUBJECT, asunto)
+        }
+        getResult.launch(Intent.createChooser(intent, "Choose an Email client :"))
+    }
+
+    private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        when (it.resultCode) {
+            Activity.RESULT_CANCELED -> {
+
+            }
+            else -> {}
+        }
     }
 
 }
